50d5b44739d7a3184a0d718a1447e39c14ca924e
Chaitanya <chaitanyasakate@gmail.com>
2026-05-21 14:40:26 +0530

    Practiced single line,multiline,Documentation comments

---
+++ src/basics/Comments.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Comments {
+    public static void main (String args[])
+    {
+ //Comments means Statements that are totally ignored by compiler & interpreter
+        System.out.println("comments types in Java");
+        System.out.println("// This is Single Line Comment for single line");
+        System.out.println("/*.....*/ This is Multi Line Comment for multi lines");
+        System.out.println("/**...*/  This is Documentation Comment for big lines");
+    }
+}
