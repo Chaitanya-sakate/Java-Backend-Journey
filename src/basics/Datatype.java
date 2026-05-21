7335a98d00472c30430c4c55d3fe32f73d5452a0
Chaitanya <chaitanyasakate@gmail.com>
2026-05-21 13:51:43 +0530

    Practiced primitive datatatype

---
+++ src/basics/Datatype.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class Datatype {
+    public static void main(String args[])
+    {
+       // Primitive datatype program
+
+        int a;
+        System.out.println("Integer datatype output is : a");
+        char b;
+        System.out.println("Character datatype output is: b");
+        float c;
+        System.out.println("float datatype output is : c");
+        double d;
+        System.out.println("double datatype output is : d");
+        short e;
+        System.out.println("short datatype output is : e");
+        long f;
+        System.out.println("long datatype is : f");
+        byte g;
+        System.out.println("byte datatype is :g ");
+        boolean h;
+        System.out.println("boolean datatype is : h");
+
+
+
+
+
+    }
+}
