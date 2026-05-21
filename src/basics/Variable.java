a30bcbdcd2f99b928540cd503f825662708b928a
Chaitanya <chaitanyasakate@gmail.com>
2026-05-21 14:24:31 +0530

    Practiced variable with rules

---
+++ src/basics/Variable.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Variable {
+    public static void main(String args[])
+    {
+        int a;
+        System.out.println("Create variable is a");
+        int _b=10;
+        System.out.println("Initialized variable a & start with '_' symbol: " + _b);
+        float $2c= 0.5f;
+        System.out.println("Variable start with '$' symbol: " + $2c);
+        double a_n=12.7777777777777;
+        System.out.println("Variable don't allow whitespace: "+ a_n);
+        short A=20;
+        System.out.println("Variable is Case Sensitive: "+ A);
+    }
+}
