68c2415d5141c3d2b64918d879ccee7f179bff7a
Chaitanya <chaitanyasakate@gmail.com>
2026-05-22 14:08:43 +0530

    feat(type-casting):implement implicit and explicit type casting

---
+++ src/basics/TypeCasting.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class TypeCasting {
+    public static void main(String args[])
+    {
+        int a=10;
+        float b = a;
+        System.out.println("Implicit TypeCasting is = "+b);
+        float c =20.7777f;
+        int d = (int)c;
+        System.out.println("Explicit TypeCasting is = "+d);
+    }
+}
