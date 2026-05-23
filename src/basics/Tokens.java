84e87c7c6106870f7001b79f9a950c53263a7584
Chaitanya <chaitanyasakate@gmail.com>
2026-05-23 10:32:39 +0530

    feat(tokens):implement Keywords,Identifier,Literal,Separator

---
+++ src/basics/Tokens.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Tokens {
+    public static void main(String args[])
+    {
+        int a ; //Keyword=int ; Identifier=a;
+        System.out.println("keyword = "+" int"+" "+" , " +"Identifier = a");
+        float b =1.5F;
+        System.out.println("Literal is F " + b);
+        double c = 1.77777777d;
+        System.out.println("Literal is d " + c);
+        System.out.println("Separator is : { }");
+
+    }
+}
