624a347371f179eaa2d271ef4b491e6b4a2aeec1
Chaitanya <chaitanyasakate@gmail.com>
2026-05-30 15:16:46 +0530

    feat(loops) : implement while loop entry control structure

---
+++ src/basics/WhileLoop.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class WhileLoop {
+    public static void main(String args[])
+    {
+        System.out.println("While Loop");
+        int n =1;
+        while(n<=10)
+        {
+            System.out.println("Java Backend");
+            ++n;
+        }
+
+    }
+}
