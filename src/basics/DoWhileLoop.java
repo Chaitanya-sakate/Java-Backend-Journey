68fbee20138cd6045240b0b02c3b42a124320fa8
Chaitanya <chaitanyasakate@gmail.com>
2026-06-01 07:31:06 +0530

    feat(do-while loop) : impement do while loop exit control structure

---
+++ src/basics/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class DoWhileLoop {
+    public static void main(String args[])
+    {
+        System.out.println("Do while loop");
+        int a=1;
+        do
+        {
+           System.out.println("Positive no = "+a);
+           ++a;
+        }
+        while(a<=10);
+
+    }
+}
