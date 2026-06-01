ae69e11a17a17a855f20e09c249c5df6830f99aa
Chaitanya <chaitanyasakate@gmail.com>
2026-06-01 07:32:12 +0530

    feat(for loop) : implement for loop

---
+++ src/basics/ForLoop.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class ForLoop {
+    public static void main(String args[])
+    {
+        System.out.println("Reverse Natural no");
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+        System.out.println("Natural no");
+        for(int n=1;n<=10;n++)
+        {
+            System.out.println(n);
+        }
+    }
+}
